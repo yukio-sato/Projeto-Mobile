f37a1f8530d99e61d0d2a70dfbe177c053d82e2d
yukio-sato <yukioutiyamasato@gmail.com>
2023-04-06 03:52:34 +0900

    \[05/04/2023\] Tentativa de fazer o paint.mp4 funcionar no mobile, somente 10% feito.

--- www/motor.js
+++ www/motor.js
@@ -1,43 +1,67 @@
-        function teste()
+        var a= 0;
+        function teste(mouse)
         {
             var blocodenotas = document.getElementById("blocoNotas");
             var q = blocodenotas.getContext("2d");
-            var stick = false;
-            blocodenotas.ontouchstart = function deu(mouse)
-            {
-                q.moveTo(mouse.clientX,mouse.clientY);
-                stick = true;
-            }
-            blocodenotas.ontouchend = function parou()
-            {
-                stick = false;
-            }
-            blocodenotas.ontouchmove = function queijo(mouse)
-            {
-                if (stick == true)
-                {
-                    q.lineTo(mouse.clientX,mouse.clientY);
-                    q.stroke();
-                }
-            }
-            
+            var linha = false;
+            var desenhar = false;
+            var tam = 4;
+            q.beginPath();
+            document.getElementById("sus").textContent = mouse;
+
+            //-------------------------------------- mouse --------------------------------------//
+
             blocodenotas.onmousedown = function deu(mouse)
             {
                 q.moveTo(mouse.clientX,mouse.clientY);
-                stick = true;
+            document.getElementById("sus").textContent = mouse;
+                linha = true;
             }
             blocodenotas.onmouseup = function parou()
             {
-                stick = false;
+                linha = false;
+            }
+            blocodenotas.onmouseleave = function parou()
+            {
+                linha = false;
             }
             blocodenotas.onmousemove = function queijo(mouse)
-            {
-                if (stick == true)
-                {
+            {   
+                a++;
+                if (linha == true)
+                {                   
                     q.lineTo(mouse.clientX,mouse.clientY);
+                    document.getElementById("sus").textContent = mouse+" movendo"+" "+a;
                     q.stroke();
+                    q.lineWidth = tam;
                 }
             }
+            
+            //-------------------------------------- Touch --------------------------------------//
+            
+            blocodenotas.ontouchstart = function mobiledeu(touch)
+            {
+                q.moveTo(touch.clientX,touch.clientY);
+            document.getElementById("sus").textContent = touch;
+                linha = true;
+            }
+            blocodenotas.ontouchend = function mobileparou()
+            { 
+             document.getElementById("sus").textContent = touch+" parou";
+            }
+            blocodenotas.ontouchcancel
+            {
+             document.getElementById("sus").textContent = touch+" parou";
+            }
+            blocodenotas.ontouchmove = function mobilequeijo(touch)
+            {            
+                a++;
+                q.lineTo(touch.clientX,touch.clientY);
+                document.getElementById("sus").textContent = touch+" movendo"+" "+a;
+                q.stroke();
+                q.lineWidth = tam;
+            }
+            
         }
     function update()
     {
